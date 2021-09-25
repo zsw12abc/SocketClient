8636b297c9f41743af543e33d2830160115b9609
Shaowei <Shaowei.Zhang@Outlook.com>
2021-09-25 20:17:34 +1000

    import socket

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HeaderComponent } from './header/header.component';
-import { OrderListComponent } from './order-list/order-list.component';
+import {AppComponent} from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {HeaderComponent} from './header/header.component';
+import {OrderListComponent} from './order-list/order-list.component';
 import {OrderCenterService} from "./shared/Order-Center.service";
+import {SocketIoConfig, SocketIoModule} from "ngx-socket-io";
+import {FormsModule} from "@angular/forms";
+
+const config: SocketIoConfig = {url: 'http://localhost:4000', options: {}};
 
 @NgModule({
   declarations: [
@@ -15,9 +19,12 @@ import {OrderCenterService} from "./shared/Order-Center.service";
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [OrderCenterService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
