523b9e1c9af0991a1e8ea070e91d41979fcafeba
Shaowei <Shaowei.Zhang@Outlook.com>
2021-09-26 09:12:08 +1000

    fix the filter

--- src/app/shared/my-filter.pipe.ts
+++ src/app/shared/my-filter.pipe.ts
@@ -1,15 +1,19 @@
 import {Pipe, PipeTransform} from '@angular/core';
 import {Order} from "./order.model";
+import {Observable} from "rxjs";
 
 @Pipe({name: 'filterPrice'})
 export class MyFilterPipe implements PipeTransform {
-  transform(items?: Order[], price?: number): Order[] {
-    if (items == null) {
-      return [];
-    }
-    if (price == null) {
+  transform(items: any, price: number): Order[] {
+    if (!items || !price) {
       return items;
     }
-    return items.filter(item => item.price == price);
+    let result = []
+    let x : Order[] = items as Order[];
+    console.log('price: ' + price)
+    for (let item of items){
+      console.log(item.item + ": " + item.price);
+    }
+    return x.filter(item => item.price.toString().indexOf(price.toString()) !== -1);
   }
 }
